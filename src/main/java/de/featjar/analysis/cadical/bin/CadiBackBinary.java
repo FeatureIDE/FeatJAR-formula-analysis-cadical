c49ea6fc806b39ea8d49f931045cecb8032eea7c
Andreas Gerasimow <andreas.gerasimow@uni-ulm.de>
2024-08-16 10:39:10 +0200

    applies autoformat

--- src/main/java/de/featjar/analysis/cadical/bin/CadiBackBinary.java
+++ src/main/java/de/featjar/analysis/cadical/bin/CadiBackBinary.java
@@ -1,11 +1,14 @@
 /*
  * Copyright (C) 2024 FeatJAR-Development-Team
  *
+ * This file is part of FeatJAR-formula-analysis-cadical.
  *
+ * formula-analysis-cadical is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
+ * formula-analysis-cadical is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
